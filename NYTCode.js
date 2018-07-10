989c79c8b738a211bed2b8b553d2406fe5434334
ItsY8S <gri.y8s@gmail.com>
2018-07-10 08:27:05 -0400

    Testing connectivity 3

--- NYTCode.js
+++ NYTCode.js
@@ -1,9 +1,11 @@
-const apiKey = '667c11f5b3ba4e6cabb53f8ceab9e20d'
+// API key
+const authKey = '667c11f5b3ba4e6cabb53f8ceab9e20d'
 
+// Based on the queryTerm we will create a queryURL
 const queryURLBase =
   'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=' +
-  apiKey +
-  '&q='
+  authKey +
+  '&q=sports'
 
 function runQuery(queryURL) {
   $.ajax({ url: queryURL, method: 'GET' }).done(function(NYTData) {
@@ -12,4 +14,6 @@ function runQuery(queryURL) {
   })
 }
 
-runQuery(queryURLBase)
+$('#runSearch').on('click', function() {
+  runQuery(queryURL)
+})
